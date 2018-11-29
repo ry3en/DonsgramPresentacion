49ece5fe414275476f6b56ba37301b8b1770edf6
ry3en <34361537+ry3en@users.noreply.github.com>
2018-11-28 21:20:03 -0600

    Revert "profiles"
    This reverts commit 47d0d1fb047a635c5e69510fc7ac97320cfe6d6c.

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,7 +3,6 @@ class UsuariosController < ApplicationController
   before_action :set_user
 
   def show
-
   end
 
   def update
@@ -14,6 +13,4 @@ private
 def set_user
   @user = User.find(params[:id])
 end
-def user_params
-			params.require(:user).permit(:email,:username,:name,:last_name,:blood_type,:cover)
-		end
+
